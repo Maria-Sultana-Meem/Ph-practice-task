e78193f57f3d39699e5fb166760274f96bb02685
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-08-01 19:05:54 +0600

    object

---
+++ Js-practice/object.js
@@ -0,0 +1,44 @@
+
+// ?For this object below add a property named passenger capacity with value 5
+/*
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car.passenger=5
+console.log(car);
+*/
+
+//? Count the number of properties.
+/*
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+let key = Object.keys(student)
+console.log(key.length);
+*/
+
+//? Loop through an object and print the key-value pairs with their types
+/*
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+
+
+for (const key in myObject) {
+   console.log(`key: ${key} | type : ${typeof myObject[key]}`);
+      
+}
+   */
+
