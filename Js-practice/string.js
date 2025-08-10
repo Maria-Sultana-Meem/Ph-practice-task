c4e43b97f18bea3c89e6144328ec2bc11e6ba16b
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-08-10 11:01:01 +0600

    string

---
+++ Js-practice/string.js
@@ -0,0 +1,50 @@
+//? Count how many times a string has the letter a
+// const letter = 'maria sultana meem';
+// let count=0;
+// for(let i =0; i<letter.length;i++){
+//     if (letter[i]==='a') {
+//         count++
+//     }
+   
+    
+// }
+//  console.log(count);
+
+// ? Count how many times a string has the letter a or A
+
+// const letter='I am a student. Apple is my favourite fruit'
+// let count = 0;
+// for(let i = 0; i<letter.length;i++){
+//     if (letter[i].toUpperCase()==='a'.toUpperCase()) {
+//         count++
+//     }
+// }
+// console.log(count);
+
+//? Check whether a string contains all the vowels a, e, i, o, u
+
+// let vowels ='i want to a web devoloper. and ypu?'
+// if (vowels=== 'a','e','i','o','u') {
+//     console.log('have');
+    
+// } else {
+//     console.log('not have');
+    
+// }
+
+//? If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+
+// let str = 'x and X';
+// str= str.replace('x' , 'y').replace('X','Y')
+// // str= str.replace('X' , 'Y')
+// console.log(str);
+
+//? Capitalize Every first Letter of each word in a String
+
+let capitalize= 'i want to a good developer';
+let str =capitalize.charAt(0).toUpperCase() + capitalize.slice(1);
+console.log(str);
+
+
+
+
